9dce6a27c00998289f1e83bc4b200b28e2211b2d
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-04-11 23:16:48 +0300

    add game lost

--- Assets/_Script/LevelView.cs
+++ Assets/_Script/LevelView.cs
@@ -7,10 +7,12 @@ public class LevelView : MonoBehaviour
 {
     [SerializeField] private List<StarView> stars;
     [SerializeField] private EndPointView endPoint;
-
+    [SerializeField] private Weight player;
+    private LevelModel model;
+    private bool isTryingCompleteLevel = false;
     public void Init(int levelIdx)
     {
-        LevelModel model = new(levelIdx);
+        model = new(levelIdx);
         stars.ForEach(s => s.onCatchCallback = () => model.starCountReactive.value++);
         endPoint.onCatchCallback = () =>
         {
@@ -21,9 +23,32 @@ public class LevelView : MonoBehaviour
     }
 
     private IEnumerator CompleteLevelWithDelay(LevelModel model)
-    {  
+    {
         yield return new WaitForSeconds(3f);
 
         GameSession.Instance.CompleteLevel(model);
+    }
+    private void FixedUpdate()
+    {
+        if (!OnScreen(player.transform.position))
+        {
+            if (!isTryingCompleteLevel)
+            {
+                isTryingCompleteLevel = true;
+                Invoke(nameof(TryCompleteLevel), 1);
+            }
+        }
+    }
+    private void TryCompleteLevel()
+    {
+        GameSession.Instance.CompleteLevel(model);
+        isTryingCompleteLevel = false;
+    }
+    private bool OnScreen(Vector3 position)
+    {
+        var screenPos = Camera.main.WorldToScreenPoint(position);
+        var cameraPos = Camera.main.WorldToScreenPoint(Camera.main.transform.position);
+
+        return screenPos.y > 0;
     }
 }
