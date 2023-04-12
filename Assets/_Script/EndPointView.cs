8da7d86936f7e31740b0c8a44f7841a3b8da986e
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-04-12 16:36:53 +0300

    add some particles

--- Assets/_Script/EndPointView.cs
+++ Assets/_Script/EndPointView.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class EndPointView : CatchableView
 {
+    [SerializeField] private ParticleSystem catchParticle;
     protected override void OnCatch(Weight player)
     {
+        catchParticle.Play();
         GetComponent<SpriteRenderer>().color = Color.green;
         player.GetComponent<SpriteRenderer>().SetActive(false);
     }
